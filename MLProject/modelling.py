60fe24b29a278d93b97499d3b99e335b0066e03a
kholilah78 <kholilahnurafifah0708@gmail.com>
2026-05-26 14:10:13 +0700

    fixxx experiment

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -132,8 +132,6 @@ def main():
     with mlflow.start_run():
 
         # Log params
-        mlflow.log_param('n_estimators',      args.n_estimators)
-        mlflow.log_param('max_depth', str(args.max_depth))
         mlflow.log_param('min_samples_split', args.min_samples_split)
         mlflow.log_param('min_samples_leaf',  args.min_samples_leaf)
         mlflow.log_param('random_state',      args.random_state)
